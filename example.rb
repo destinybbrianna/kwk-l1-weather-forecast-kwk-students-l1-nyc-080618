3299378c30aa59d8d4977082f27b539c9938411b
destinybbrianna <destinybbrianna@gmail.com>
2018-08-09 17:23:39 +0000

    automatically backed up by learn

---
+++ example.rb
@@ -0,0 +1,15 @@
+
+shopping_items = ["apple" , "banana" , "carrot"]
+price= [1,4,3]
+
+shopping_hash = {}
+
+counter = 0 
+
+shopping_items.each do |rochelle|
+  shoppping_hash[rochelle] = prices[counter]
+  counter += 1 
+end
+
+puts shopping_hash
+
